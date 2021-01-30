66cecd241f38e25a7695b22c2b14c5b2c371680b
laurarantonen <laura.h.rantonen@gmail.com>
2021-01-30 22:54:42 +0200

    Add animation for idle and running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,16 @@ public class PlayerMovement : MonoBehaviour
     public bool canMove;
     public float moveSpeed;
     private Rigidbody2D rb;
+    private bool facingRight = true;
     private float moveDirection;
+
+    private Animator anim;
 
     // Called after all objects are initialized.
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Get Rigidbody2D component
+        anim = GetComponent<Animator>();
         canMove = true;
     }
     
@@ -22,9 +26,41 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveDirection = Input.GetAxis("Horizontal");
+        Process();
+        
+        //Animate
+        if (moveDirection > 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        
         
         // Moves the player
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+
+    private void Process()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        if (moveDirection == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+    }
+    
+    
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }

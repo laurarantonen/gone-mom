67b9c0c96aa487eef3ec11308af601bdc3f7af02
laurarantonen <laura.h.rantonen@gmail.com>
2021-01-30 23:31:14 +0200

    Refactored turn and continue into separate prefabs

--- Assets/Scripts/TurnController.cs
+++ Assets/Scripts/TurnController.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransition : MonoBehaviour
+public class TurnController : MonoBehaviour
 {
 
     public string goToAisleScene;
-    public string goForwardScene;
     private bool playerInsideTrigger;
-    
-    //public GameObject guideText;
-    //public GameObject guideText2;
-    
+
     public void Update()
     {
         if(playerInsideTrigger && Input.GetKeyDown(KeyCode.W)){
             SceneManager.LoadScene(goToAisleScene);
-        }
-        if(playerInsideTrigger && Input.GetKeyDown(KeyCode.A)){
-            SceneManager.LoadScene(goForwardScene);
         }
     }
 
@@ -35,15 +26,11 @@ public class SceneTransition : MonoBehaviour
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             playerInsideTrigger = true;
-            //guideText.SetActive(true);
-            //guideText2.SetActive(true);
         }
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
         playerInsideTrigger = false;
-        //guideText.SetActive(false);
-        //guideText2.SetActive(false);
     }
 }

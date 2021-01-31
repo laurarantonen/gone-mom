be123c90abe83ae7259145a3ebe52a92910a0b1a
laurarantonen <laura.h.rantonen@gmail.com>
2021-01-31 17:55:26 +0200

    Last minute bug fixing

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    private SceneFader sceneFader;
+
+    public void Start()
+    {
+        sceneFader = FindObjectOfType<SceneFader>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Hello");
+            sceneFader.FadeToLevel();
+            Invoke("GoToScene", 2f);
+        }
+    }
+    
+    void GoToScene()
+    {
+        SceneManager.LoadScene("EndingScene");
+    }
+    
+    
+}

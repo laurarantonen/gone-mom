23d8acdc45ca64a039c85d237d455aab47340ce3
laurarantonen <laura.h.rantonen@gmail.com>
2021-01-31 14:06:41 +0200

    Add music player prefab for background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+ 
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+ 
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}

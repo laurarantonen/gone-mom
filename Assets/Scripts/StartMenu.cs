d0b3d454decc50995ef60a53865af5c0ebfe64c3
VirpiV <56123123+VirpiV@users.noreply.github.com>
2021-01-31 15:03:17 +0200

    Added the Start Menu scene and a startmenu script

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("Toys");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
+
